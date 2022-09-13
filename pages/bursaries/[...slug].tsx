31d51deae4f3314af31de26baca161d46345c62c
KevinRaleie-dev <kkevraleie@gmail.com>
2022-09-13 12:38:23 +0200

    no longer fetching via ssr, takes too long. added swr for client side fetching instead

--- pages/bursaries/[...slug].tsx
+++ pages/bursaries/[...slug].tsx
@@ -1,11 +1,11 @@
-import { GetServerSideProps } from "next"
-import axios from "axios"
-import { Nav } from "../../components/Nav";
+import axios from "axios";
+import { GetServerSideProps } from "next";
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { FiArrowLeft, FiShare2 } from 'react-icons/fi';
+import useSwr from 'swr';
 import { Feed } from "../../components/Feed";
-import Head from 'next/head'
-import { useRouter } from 'next/router'
-
-import { FiArrowLeft, FiShare2 } from 'react-icons/fi'
+import { Nav } from "../../components/Nav";
 
 async function handler(month: string) {
     try {
@@ -22,70 +22,85 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
     const slug = context.params?.slug
 
-    const data = await handler(String(slug))
-
-    if (data?.response?.data?.success === false) {
-        return {
-            notFound: true
-        }
-    }
-
     return {
         props: {
-            data,
             slug
         }
     }
 }
 
 type BursaryProps = {
-    data: any,
     slug: string
 }
 
-const Bursary = ({ data, slug }: BursaryProps) => {
+const Bursary = ({ slug }: BursaryProps) => {
     const router = useRouter()
-
-    console.log(router.asPath)
+    const { data, error } = useSwr(slug, handler)
 
     const shareHandler = async () => {
         await navigator.share({
             url: `https://bursary-findr.vercel.com${router.asPath}`,
-            title: data.title,
+            title: data?.title,
             text: `Find bursaries closing in the month of ${slug}`
         })
     }
 
+    if (!error && !data) {
+        return (
+            <div className="mt-10 text-center">
+                <p>
+                    Fetching bursaries over the interwebs...
+                </p>
+            </div>
+        )
+    }
+
+    if (error) {
+        return <>{error}</>
+    }
+
     return (
-        <div>
-            <Head>
-                <title>
-                    findr. - {data.title}
-                </title>
-            </Head>
-            <header
-                className="top-0 sticky z-10 bg-white"
-            >
-                <Nav />
-            </header>
+        <>
             <div>
-                <div className="flex space-x-3 px-5 items-center">
-                    <div
-                        onClick={() => router.back()}
-                        className="p-2 border rounded-md">
-                        <FiArrowLeft />
+                <Head>
+                    <title>
+                        findr. - {data?.title}
+                    </title>
+                </Head>
+                <header
+                    className="top-0 sticky z-10 bg-white"
+                >
+                    <Nav />
+                </header>
+                <div>
+                    <div className="flex space-x-3 px-5 items-center">
+                        <div
+                            onClick={() => router.back()}
+                            className="p-2 border rounded-md">
+                            <FiArrowLeft />
+                        </div>
+                        <div
+                            onClick={shareHandler}
+                            className="p-2 border rounded-md">
+                            <FiShare2 />
+                        </div>
                     </div>
-                    <div 
-                    onClick={shareHandler}
-                    className="p-2 border rounded-md">
-                        <FiShare2 />
+                    <div>
+                        {
+                            data?.message ? (
+                                <p className="text-center mt-10 font-medium">
+                                    😬 Looks like bursaries for this month are not available yet
+                                </p>
+                            ) : (
+                                <Feed
+                                    data={data}
+                                />
+                            )
+                        }
                     </div>
                 </div>
-                <Feed
-                    data={data}
-                />
             </div>
-        </div>
+        </>
     )
 }
 
